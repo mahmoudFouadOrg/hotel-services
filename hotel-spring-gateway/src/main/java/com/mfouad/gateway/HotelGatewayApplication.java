342495e91038f8bdc165253a902f7f57b21da044
mahmoud fouad <mahmoud.ah.fouad@gmail.com>
2025-07-23 22:44:29 +0300

    replace quarkus gateway with spring boot

---
+++ hotel-spring-gateway/src/main/java/com/mfouad/gateway/HotelGatewayApplication.java
@@ -0,0 +1,23 @@
+package com.mfouad.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HotelGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelGatewayApplication.class, args);
+	}
+	
+	@RequestMapping("/fallback")
+    public ResponseEntity<String> fallback() {
+        return new ResponseEntity<>("Service is temporarily unavailable. Please try again later.", HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
+}
---
+++ hotel-spring-gateway/src/test/java/com/mfouad/gateway/HotelGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfouad.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
