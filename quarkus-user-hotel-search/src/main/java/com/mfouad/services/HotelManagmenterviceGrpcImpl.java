f0617b07db1b7d6031e9620a9440cb9ff1cb742b
mahmoud fouad <mahmoud.ah.fouad@gmail.com>
2025-07-26 19:59:47 +0300

    create micro srevice for hotel search

---
+++ quarkus-user-hotel-search/src/main/java/com/mfouad/services/HotelManagmenterviceGrpcImpl.java
@@ -0,0 +1,34 @@
+package com.mfouad.services;
+
+import org.jboss.logging.Logger;
+
+import com.mfouad.proto.Hotel;
+import com.mfouad.proto.HotelAcDetivate;
+import com.mfouad.proto.HotelManagmenterviceGrpc.HotelManagmenterviceImplBase;
+import com.mfouad.proto.HotelStatus;
+
+import io.grpc.stub.StreamObserver;
+import io.quarkus.grpc.GrpcService;
+
+@GrpcService
+public class HotelManagmenterviceGrpcImpl extends HotelManagmenterviceImplBase{
+
+	
+	Logger log= Logger.getLogger(HotelManagmenterviceGrpcImpl.class);
+	
+	@Override
+	public void addNewHotel(Hotel request, StreamObserver<HotelStatus> responseObserver) {
+		// TODO Auto-generated method stub
+		log.info("recive rquest "+request.getCountryId());
+		responseObserver.onNext(HotelStatus.newBuilder().setStatus("created").build()); 
+		
+		super.addNewHotel(request, responseObserver);
+	}
+	
+	
+	@Override
+	public void acDeActivate(HotelAcDetivate request, StreamObserver<HotelStatus> responseObserver) {
+		// TODO Auto-generated method stub
+		super.acDeActivate(request, responseObserver);
+	}
+}
